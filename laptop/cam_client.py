7c22d76ce51015ab9c11d752cdaf075ee53a964a
Phelipe <92000572+Phelipe-J@users.noreply.github.com>
2024-12-05 05:11:41 -0300

    Update cam_client.py
    Corrigi os negócio que tava escrito adress
    O código testa o ping logo quando inicia
    no fim cria um arquivo que guarda o ping e as perdas de pacote

--- laptop/cam_client.py
+++ laptop/cam_client.py
@@ -2,27 +2,38 @@ import cv2
 import socket
 import struct
 import numpy as np
+import time
 
 MAX_DGRAM = 2**16
 COUNT_LIMIT = 255
 
 # Configurar o socket UDP
-server_adress = ("localhost", 5005)
-client_adress = ("localhost", 5080)
+server_address = ("localhost", 5005)
+client_address = ("localhost", 5080)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.bind(client_adress)
+client.bind(client_address)
 
-print(f"Client iniciado em {client_adress[0]}:{client_adress[1]}. Aguardando cliente...")
+print(f"Client iniciado em {client_address[0]}:{client_address[1]}. Aguardando cliente...")
 
-client.sendto("Estabelecendo conexao client -> server".encode("utf-8"), server_adress)
+client.sendto("Estabelecendo conexao client -> server".encode("utf-8"), server_address)
+
+start_time = time.perf_counter()
+
+msg, address = client.recvfrom(1024)
+end_time = time.perf_counter()
+ping = (end_time - start_time)*1000
+
+print(f"Ping: {ping:.3f} ms")
+
+print(msg.decode('utf-8'))
 
 buffer = b''
 expected_sequence_number = 0
 lost_packets_count = 0
 
 while True:
-    message, adress = client.recvfrom(MAX_DGRAM)
+    message, address = client.recvfrom(MAX_DGRAM)
 
     sequence_number = struct.unpack("BB", message[0:2])[0]
     if expected_sequence_number != sequence_number:
@@ -43,5 +54,10 @@ while True:
 
 
 print(f"Ocorreram {lost_packets_count} perdas de pacote!")  
+
+with open("dados.txt", "w") as arquivo:
+    arquivo.write(f"Ping: {ping:.3f} ms\n")
+    arquivo.write(f"Lost Packets: {lost_packets_count}")
+
 cv2.destroyAllWindows()
 client.close()
