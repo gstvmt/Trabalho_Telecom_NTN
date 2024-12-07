237416967cdb81c297b2714e57843ac59bcf957e
gstvmt <gumotabarros@gmail.com>
2024-12-07 11:38:40 -0300

    camera working through network

--- run.sh
+++ run.sh
@@ -6,5 +6,17 @@ LOCAL_ROS_PKG_DIR=$(pwd)/ros_pkg
 # Nome da imagem Docker
 IMAGE_NAME="raspicam_ros"
 
+# Allow local connections to the X server for GUI applications in Docker
+xhost +local:
+
+# Setup for X11 forwarding to enable GUI
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 # Execute o container
-docker run -it --rm --privileged --device /dev/video0:/dev/video0 -e DISPLAY=$DISPLAY -v ./src:/root/ros_ws/src -v /dev:/dev $IMAGE_NAME bash
+docker run -it --rm --privileged --device /dev/video0:/dev/video0 -e DISPLAY=$DISPLAY \
+    -v ./src:/root/ros_ws/src -v /dev:/dev --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" $IMAGE_NAME bash
