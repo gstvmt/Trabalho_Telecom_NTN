450dcbbe6cb5d3bd67c1cd7451c399c1aca162db
Phelipe <phelipejar@hotmail.com>
2024-12-10 02:04:06 -0300

    Revert "Documentação do código"
    This reverts commit c61ddf0bd36286e71e1687bac32f77f1c072e495.

--- laptop/cam_client.py
+++ laptop/cam_client.py
@@ -6,20 +6,16 @@ import time
 import os
 from datetime import datetime
 
-MAX_DGRAM = 2**16           # Tamanho máximo de datagrama UDP
-COUNT_LIMIT = 255           # Limite do contador da sequência
+MAX_DGRAM = 2**16
+COUNT_LIMIT = 255
 
-# Variáveis para controle
-buffer = b''                        # Buffer de reconstrução da imagem
-expected_sequence_number = 0        # Número da sequência esperado, enviado pelo servidor
-bandwidth_start_time = time.time()  # Tempo inicial para cálculo da largura de banda
-last_packet_time = time.time()      # Tempo do último pacote recebido
-max_bandwidth = 21                  # largura de banda máxima disponível
-last_save_time = time.time()        # Controle de gravação de métricas
-frame_start_time = time.time()      # Tempo inicial para cálculo de fps
+buffer = b''
+expected_sequence_number = 0
+lost_packets_count = 0
+total_data_received = 0
+bandwidth_start_time = time.time()
 
 # Variáveis para métricas
-<<<<<<< Updated upstream
 frame_start_time = time.time()
 frames_received = 0
 jitter_list = []
@@ -29,30 +25,17 @@ last_save_time = time.time()  # Controle de gravação de métricas
 decode_times = [] # decodificação
 latencies = [] # latência
 ping_list = []
-=======
-total_data_received = 0             # Quantidade de bytes recebidos no total
-lost_packets_count = 0              # Quantidade de perdas de pacote
-frames_received = 0                 # Quantidade de quadros recebidos
-jitter_list = []                    # Lista dos valores de jutter coletados
-latencies = []                      # Lista dos valores de latência coletados
-decode_times = []                   # Lista dos tempos de codifição coletados
->>>>>>> Stashed changes
 
 # Configurar o socket UDP
-server_address = ("localhost", 5005)        # Endereço do servidor
-client_address = ("localhost", 5080)        # Enderedo do client
+server_address = ("localhost", 5005)
+client_address = ("localhost", 5080)
 
-# Incialização do socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind(client_address)
 
 print(f"Client iniciado em {client_address[0]}:{client_address[1]}. Aguardando cliente...")
 
-<<<<<<< Updated upstream
 client.sendto("Estabelecendo conexao client -> server".encode("utf-8"), server_address)
-=======
-client.sendto(struct.pack("d", time.time()), server_address)        # Avisa a conexão para o servidor e envia o tempo para sincronização
->>>>>>> Stashed changes
 
 # Calcula o Ping ao iniciar a conexão
 ping_start_time = time.perf_counter()
@@ -66,11 +49,10 @@ print(msg.decode('utf-8'))
 
 while True:
 
-    # Recebe a mensagem do servidor
+    # Recebe mensagem
     message, address = client.recvfrom(MAX_DGRAM)
-    total_data_received += len(message)             # Define o tamanho da mensagem recebida
+    total_data_received += len(message)
 
-<<<<<<< Updated upstream
     # Latência
     if len(message) == 8:  # Um pacote de timestamp terá exatamente 8 bytes
         # Processar o timestamp
@@ -86,29 +68,11 @@ while True:
     if expected_sequence_number != sequence_number:
         #comentei a linha abaixo pra não ficar floodando o terminal
        #print(f"Perda de pacote detectada. Esperado: {expected_sequence_number}, Recebido: {sequence_number}")  
-=======
-    header_size = struct.calcsize("BBd")            # Tamanho do cabeçalho do pacote (sequência, segmentos restates, timestamp)
-    sequence_number, segments_left, server_timestamp = struct.unpack("BBd", message[:header_size])      # Desempacota a mensagem e salva cada parte nas respectivas variáveis
-
-    # Calcula a latência e salva na lista
-    if time.time() - server_timestamp > 0:
-        latencies.append(time.time() - server_timestamp)
-        #print(time.time() - server_timestamp)
-
-    # Verifica se a sequência que o client tem é a mesma do servidor. Se não for, houve perda de pacote, então isso é registrado.
-    if expected_sequence_number != sequence_number:
-        #print(f"Perda de pacote detectada. Esperado: {expected_sequence_number}, Recebido: {sequence_number}")  
->>>>>>> Stashed changes
         lost_packets_count += 1
 
-    expected_sequence_number = (expected_sequence_number + 1) % COUNT_LIMIT     # Redefine a sequência do client para ser a mesma do servidor, para corrigir no caso de dessincronia
+    expected_sequence_number = (expected_sequence_number + 1) % COUNT_LIMIT
 
-<<<<<<< Updated upstream
     # Jitter
-=======
-    
-    # Calcula o jitter e salva na lista
->>>>>>> Stashed changes
     current_time = time.time()
     jitter = abs(current_time - last_packet_time)
     jitter_list.append(jitter)
@@ -120,7 +84,6 @@ while True:
     else:
         buffer += message[2:]
         decode_start = time.time()
-<<<<<<< Updated upstream
         # Alinha abaixo foi alterada por: DeprecationWarning: The binary mode of fromstring is deprecated, as it behaves surprisingly on unicode inputs. Use frombuffer instead
         # img = cv2.imdecode(np.fromstring(buffer, dtype=np.uint8), 1)
         img = cv2.imdecode(np.frombuffer(buffer, dtype=np.uint8), 1)
@@ -133,30 +96,8 @@ while True:
         buffer = b''
         if cv2.waitKey(1) == 27:
             break
-=======
-        img = cv2.imdecode(np.frombuffer(buffer, dtype=np.uint8), 1)        # Decodifica a imagem
-        
-        # Calcula o tempo de decodificação e armazena o resultado
-        decode_time = time.time() - decode_start
-        decode_times.append(decode_time)
 
-        # Mostra o quadro decodificado
-        try:
-            cv2.imshow("frame", img)    # Mostra a imagem
-            frames_received += 1        # Aumenta o número de frames recebidos
-            buffer = b''                # Limpa o buffer
-            if cv2.waitKey(1) == 27:    # Caso a tecla esc for pressionada fecha o programa
-                break
-        except Exception as e:          # No caso de algum erro com a exibição da image, imprime a falha, limpa o buffer e continua
-            print(f"Falha: {e}")
-            buffer = b''
-            continue
-
-
-    
->>>>>>> Stashed changes
-
-    # Calcula a largura de banda usada na rede
+    # Largura de banda
     elapsed_time = time.time() - bandwidth_start_time
     if elapsed_time >= 5:  # Atualizar a cada 5 segundos
         bandwidth = (total_data_received / (1024 * 1024)) / elapsed_time  # MB/s
@@ -166,12 +107,11 @@ while True:
         bandwidth_start_time = time.time()
 
     # FPS Real
-    if time.time() - frame_start_time >= 1:         # Ocorre a cada segundo
+    if time.time() - frame_start_time >= 1:
         #print(f"FPS Recebidos: {frames_received}")
         frames_received = 0
         frame_start_time = time.time()
 
-<<<<<<< Updated upstream
     # Ping
     if expected_sequence_number == 0:
         client.sendto("ping".encode("utf-8"), server_address)
@@ -188,11 +128,6 @@ while True:
     # Salvar métricas no arquivo
     if time.time() - last_save_time >= 20:
         with open("metricas.txt", "a") as arquivo:  # "a" para anexar dados
-=======
-    # Anexa as métricas no arquivo
-    if time.time() - last_save_time >= 10:      # Ocorre a cada 10 segundos
-        with open("metricas.txt", "a") as arquivo:
->>>>>>> Stashed changes
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             arquivo.write(f"\n[Dados registrados em: {current_time}]\n")
             arquivo.write(f"Ping Médio: {np.mean(ping_list):.3f} ms\n")
@@ -204,11 +139,6 @@ while True:
             arquivo.write(f"FPS Recebidos: {frames_received}\n")
         last_save_time = time.time()
 
-<<<<<<< Updated upstream
-=======
-    
-# Finalização do client
->>>>>>> Stashed changes
 print(f"Ocorreram {lost_packets_count} perdas de pacote!")  
 file_path = os.path.join(os.getcwd(), "metricas.txt")
 print(f"Salvando o arquivo de metricas em: {file_path}")
--- raspi/cam_server.py
+++ raspi/cam_server.py
@@ -5,87 +5,62 @@ import math
 import time
 
 # Configurar o socket UDP
-UDP_IP = "localhost"                # Endereço IP do servidor
-UDP_PORT = 5005                     # Porta do servidor
+UDP_IP = "localhost"
+UDP_PORT = 5005
 
-MAX_DGRAM = 2**16                   # Tamanho máximo de datagrama UDP
-MAX_IMAGE_DGRAM = MAX_DGRAM - 64    # Tamanho máximo para os pacotes de imagem
-CONT_LIMIT = 255                    # Limite do contador da sequência
+MAX_DGRAM = 2**16
+MAX_IMAGE_DGRAM = MAX_DGRAM - 64
+CONT_LIMIT = 255
 
-# Inicialização do socket UDP
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((UDP_IP, UDP_PORT))
 
 print(f"Servidor iniciado em {UDP_IP}:{UDP_PORT}. Aguardando cliente...")
 
 # Esperar pelo primeiro cliente
-<<<<<<< Updated upstream
 msg, address = server.recvfrom(1024)
 server.sendto("Conexao estabelecida.".encode("utf-8"), address)
 print(f"Cliente conectado: {address}")
 print(msg.decode("utf-8"))
-=======
-msg, address = server.recvfrom(1024)        # Recebe uma mensagem inicial do client (que é o tempo atual do client), indicando a conexão
-print(f"Cliente conectado: {address}")
 
-# Sincronização de tempo entre client e servidor
-client_time = struct.unpack("d", msg)[0]    # Armazena o tempo do relógio do client
-time_offset = client_time - time.time()     # Calcula a diferença de tempo entre os dois relógios
->>>>>>> Stashed changes
+cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)                   # Acessa a câmera de índice 0
-
-# Verifica se a câmera foi aberta com sucesso
 if not cap.isOpened():
     print("Erro ao abrir a câmera")
     server.close()
     exit()
 
-sequence_number = 0        # Inicialização da sequência dos pacotes
+sequence_number = 0
 
 try:
     while True:
-        ret, frame = cap.read()        # Captura 1 frame da câmera
-        if not ret:                    # Verifica se a captura foi bem sucedida
+        ret, frame = cap.read()
+        if not ret:
             print("Erro ao capturar frame")
             break
 
-        # Codifica o frame no formato JPEG
         _, buffer = cv2.imencode('.jpg', frame)
+        # Alinha abaixo foi alterada por: DeprecationWarning: tostring() is deprecated. Use tobytes() instead. buffer = buffer.tostring()
+        # buffer = buffer.tostring()
         buffer = buffer.tobytes()
 
-<<<<<<< Updated upstream
         # Enviar o timestamp como pacote separado
         timestamp = time.time()
         timestamp_data = struct.pack("d", timestamp)
         server.sendto(timestamp_data, address)
 
         # Enviar a imagem segmentada
-=======
-        # Segmenta a imagem de acordo com o tamanho do buffer
->>>>>>> Stashed changes
         size = len(buffer)
         num_of_segments = math.ceil(size / MAX_IMAGE_DGRAM)
         start = 0
 
-<<<<<<< Updated upstream
         while num_of_segments:
             end = min(size, start + MAX_IMAGE_DGRAM)
             packet = struct.pack("BB", sequence_number, num_of_segments) + buffer[start:end]
-=======
-        timestamp = time.time() + time_offset        # Calcula o timestamp sincronizado com o relógio do client
-
-        # Envia os segmentos da imagem
-        while num_of_segments:
-            end = min(size, start + MAX_IMAGE_DGRAM)        # Define o segmento atual
-            # Cria o pacote com a sequência, número de segmentos restantes e o timestamp e envia para o client
-            packet = struct.pack("BBd", sequence_number, num_of_segments, timestamp) + buffer[start:end]
->>>>>>> Stashed changes
             server.sendto(packet, address)
             start = end
             num_of_segments -= 1
             sequence_number = (sequence_number + 1) % CONT_LIMIT
-<<<<<<< Updated upstream
         
         # A cada 256 sequencias, calcula o ping
         if sequence_number == 0:
@@ -96,11 +71,8 @@ try:
             tempo_teste = (time_test_end - time_test_end)*1000
             print(f"{tempo_teste:.5f}\n")
 
-=======
->>>>>>> Stashed changes
 
 finally:
-    # Finaliza a captura de vídeo liberando a câmera, e fecha o servidor.
     cap.release()
     server.close()
     print("Servidor encerrado")
